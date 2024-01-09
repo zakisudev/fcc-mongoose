3f40519cd609adc63054acd9f4393bb7654ff6ee
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-09 17:56:43 +0300

    Refactor findPersonById function to use Person.findById

--- myApp.js
+++ myApp.js
@@ -49,7 +49,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
