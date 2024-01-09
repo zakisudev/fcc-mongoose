92658cea66d702bd6b5c0797273f36b3d78bda0a
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-09 17:37:17 +0300

    Refactor findPeopleByName function to use Person.find method

--- myApp.js
+++ myApp.js
@@ -35,7 +35,10 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
