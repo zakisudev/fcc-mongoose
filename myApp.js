fac07fefa087d642310f422c44419e7995f1a705
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-09 17:24:08 +0300

    Refactor mongoose model instantiation

--- myApp.js
+++ myApp.js
@@ -1,20 +1,18 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-let Person;
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const personSchema = mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: { type: String, required: true },
   age: Number,
   favoriteFoods: [String],
 });
 
-Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = function (done) {
   const janeFonda = new Person({
