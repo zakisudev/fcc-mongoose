15d7d7b736ded62fd256bfa67c6ef2d515e1c3ed
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-09 14:47:53 +0300

    Add person schema to myApp.js

--- myApp.js
+++ myApp.js
@@ -7,6 +7,15 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const personSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+Person = mongoose.model('Person', personSchema);
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
