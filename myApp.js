bb6aa3caf3cfa71620573f774980360176cad224
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-09 17:50:59 +0300

    Refactor findOneByFood function to use Person.findOne method

--- myApp.js
+++ myApp.js
@@ -42,7 +42,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
